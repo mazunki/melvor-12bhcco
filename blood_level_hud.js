816c3727296c7cf438f153dd1c8c09a645baf0bd
mazunki <rolferen@gmail.com>
2022-01-27 23:07:07 +0100

    added blood hud

---
+++ blood_level_hud.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         Blood Hud
+// @namespace    melvor_hcco
+// @version      0.0.1
+// @description  Adds a blood hud for low hp levels
+// @author       mazunki
+// @match	https://*.melvoridle.com/*
+// @exclude	https://wiki.melvoridle.com*
+// @grant       none
+// @noframes
+/* globals jquery, $, game, getPlayerCombatLevel, skillLevel, CONSTANTS */
+// ==/UserScript==
+
+// Made for version 1.0
+
+let has_initialized = false;
+
+let do_add_blood_hud = function () {
+	if ($("m-page-loader.d-none").length != 0) return;
+
+	let main_container = $("#main-container")[0];
+
+	let scw = $(".simplebar-content-wrapper")[0];
+	if (scw !== undefined) {
+		let s = (scw.style === undefined)? "" : scw.style;
+		scw.style = "height: 100%; background: transparent !important; overflow: hidden scroll;";
+	}
+
+    if ($("#page-container")[0] !== undefined && player != undefined) {
+        let color = "red";
+        let missing_hp = (player.hitpointsPercent) ? (100 - player.hitpointsPercent) : 0;
+
+        let colour = (missing_hp > 50)? "#FF0000" : "#770";
+        let percenthud = 0.2;
+
+        let linear_percent = (missing_hp > 20)? 100 - missing_hp * percenthud : 100;
+
+        let hudstyle = "linear-gradient(to left, transparent " + linear_percent + "%, " + colour +")";
+        hudstyle += ", linear-gradient(to right, transparent " + linear_percent + "%, " + colour +")";
+        hudstyle += ", linear-gradient(to bottom, transparent " + linear_percent + "%, " + colour +")";
+        hudstyle += ", linear-gradient(to top, transparent " + linear_percent + "%, " + colour +")";
+
+        $("#page-container")[0].style = "padding-left: 110px; background: " + hudstyle + ";";
+    }
+}
+
+console.log("[melvor_hcco/blood_hud] Loading");
+setInterval(do_add_blood_hud, 100);
+
+console.log("[melvor_hcco/blood_hud] Done");
